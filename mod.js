8af3c70258eae65ca7582e524d5b9312f0a4eee6
zhuqiuyang <zqyhit@qq.com>
2020-10-15 21:35:45 +0800

    add index.html

---
+++ mod.js
@@ -0,0 +1,18 @@
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
+
+
+const router = new Router();
+router
+  .get("/", async (context) => {
+    await send(context, context.request.url.pathname, {
+      root: `${Deno.cwd()}/dist`,
+      index: "index.html",
+    });
+  });
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// console.log(`${Deno.cwd()}`)
+
+await app.listen({ port: 8080 });
